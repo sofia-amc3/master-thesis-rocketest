cb73762aeab7fb090f9f9958fd87e407027d32da
Sofia Canelas <1200185@isep.ipp.pt>
2023-05-09 17:56:10 +0100

    Small Page Fixes

--- sql/InsertTestUser.sql
+++ sql/InsertTestUser.sql
@@ -1,7 +1,9 @@
+-- Insert UX Researcher
 INSERT INTO public."Users"(
 	email, password, name, type)
 	VALUES ('test0','test','UX Researcher Name', 0);
 
+-- Insert Tester
 INSERT INTO public."Users"(
 	email, password, name, type)
 	VALUES ('test1','test','Tester Name', 1);
