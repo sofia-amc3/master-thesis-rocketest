e07f9cff8696bac652410e12e8cc9f635e9548be
Sofia Canelas <1200185@isep.ipp.pt>
2023-05-21 21:54:19 +0100

    #13 Added 'About the App' Slider in Side Menu

--- src/components/SideMenuAuth.tsx
+++ src/components/SideMenuAuth.tsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/styles/app.module.css";
-import { useRouter } from "next/router";
 
 const SideMenuAuth = () => {
-  const router = useRouter();
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [isIntervalActive, setIsIntervalActive] = useState(true); // to check if automated sliding is active or not
+  const slides = [
+    <>
+      Rocketest has been designed to streamline usability testing processes and
+      make them more efficient. This application aims to help user experience
+      designers and researchers spend{" "}
+      <strong className={styles.strongMedium}>less time on logistics</strong>{" "}
+      and{" "}
+      <strong className={styles.strongMedium}>
+        more time on actually testing and improving their designs
+      </strong>
+      .
+    </>,
+    <>
+      Whether you&apos;re testing a new product or a website, Rocketest provides
+      various templates that will help you to get{" "}
+      <strong className={styles.strongMedium}>
+        accurate and meaningful results in no time
+      </strong>
+      , making it the go-to tool for anyone looking to optimize their usability
+      tests.
+    </>,
+  ];
+
+  useEffect(() => {
+    let interval: any;
+
+    if (isIntervalActive) {
+      interval = setInterval(() => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+      }, 7000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isIntervalActive]);
+
+  const handleClickSlide = (slideIndex: React.SetStateAction<number>) => {
+    setCurrentSlide(slideIndex);
+    setIsIntervalActive(false); // stops the automatic slider whenever the user clicks on its buttons
+    setTimeout(() => {
+      setIsIntervalActive(true); // waits for 5 seconds before turning on the automatic sliding again
+    }, 5000);
+  };
+
   return (
     <div className={styles.sideMenuAuth}>
       {/* LOGO */}
@@ -17,23 +62,21 @@ const SideMenuAuth = () => {
           height={50}
         />
       </Link>
-      {/* Introduction Text */}
+
+      {/* Introduction Text - Slider */}
       <h5>Why Rocketest?</h5>
-      <p>
-        Rocketest has been designed to streamline usability testing processes
-        and make them more efficient. This application aims to help user
-        experience designers and researchers spend{" "}
-        <strong className={styles.strongMedium}>less time on logistics</strong>{" "}
-        and{" "}
-        <strong className={styles.strongMedium}>
-          more time on actually testing and improving their designs
-        </strong>
-        . <br></br>
-        <br></br>Whether you&apos;re testing a new product or a website,
-        Rocketest provides various templates that will help you to get accurate
-        and meaningful results in no time, making it the go-to tool for anyone
-        looking to optimize their usability tests.
-      </p>
+      <p>{slides[currentSlide]}</p>
+      {/* Slider Buttons */}
+      <div className={styles.sliderButtonsContainer}>
+        {slides.map((_, index) => (
+          <button
+            key={index}
+            className={currentSlide === index ? styles.activeSliderButton : ""}
+            onClick={() => handleClickSlide(index)}
+          />
+        ))}
+      </div>
+
       <div className={styles.bottomContainer}>
         {/* Social Media */}
         <div className={styles.socialMediaContainer}>
@@ -62,6 +105,7 @@ const SideMenuAuth = () => {
             />
           </a>
         </div>
+
         {/* Links */}
         <div className={styles.linksContainer}>
           <Link href="/termsAndConditions">Terms and Conditions</Link>
