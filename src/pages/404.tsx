2818c0df90cf71d8792fd43b883cf32b2827b99f
Sofia Canelas <1200185@isep.ipp.pt>
2023-05-27 14:30:09 +0100

    #24 Added Custom 404 Page (Not Finished)

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const PageNotFound = () => {
+  return (
+    <>
+      <Head>
+        <title>404 Page Not Found | Rocketest</title>
+      </Head>
+      <main>404 page</main>
+    </>
+  );
+};
+
+export default PageNotFound;
