ee2611e5f889fa2c5d50d42b8843d017496eb376
Sofia Canelas <1200185@isep.ipp.pt>
2023-05-29 16:44:30 +0100

    #24 Custom 404 Page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,43 @@
 import React from "react";
 import Head from "next/head";
+import styles from "@/styles/app.module.css";
+import Button from "@/components/Button";
+import { useRouter } from "next/router";
 
 const PageNotFound = () => {
+  const router = useRouter();
+
+  const backToIndex = () => {
+    router.push("/");
+  };
+
   return (
     <>
       <Head>
-        <title>404 Page Not Found | Rocketest</title>
+        <title>404 Error | Rocketest</title>
       </Head>
-      <main>404 page</main>
+      <main>
+        <div className={styles.error404Container}>
+          <div className={styles.leftContent}>
+            {/* white background */}
+            <div></div>
+            <img src="/icons/astronaut-404.svg" alt="Astronaut Illustration" />
+          </div>
+          <div className={styles.rightContent}>
+            <span className={styles.redText01}>404</span>
+            <span className={styles.redText02}>Oops! Page Not Found.</span>
+            <span className={styles.blueText}>
+              Click below to return to the application.
+            </span>
+            <Button
+              size="large"
+              text="Go Back"
+              type="primary"
+              function={backToIndex}
+            />
+          </div>
+        </div>
+      </main>
     </>
   );
 };
