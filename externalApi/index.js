d066254b198ba9ce11d5c9a6e5f81d87bf8b9707
Sofia Canelas <1200185@isep.ipp.pt>
2023-06-01 16:35:56 +0100

    #4 Added fake users

--- externalApi/index.js
+++ externalApi/index.js
@@ -1,36 +1,18 @@
-const usersLinkedin = [
-  { name: "user1", email: "user1@testeLinkedin.com" },
-  { name: "_user2", email: "user2@testeLinkedin.com" },
-  { name: "_user3", email: "user3@testeLinkedin.com" },
-  { name: "_user4", email: "user4@testeLinkedin.com" },
-  { name: "user5", email: "user5@testeLinkedin.com" },
-  { name: "_user6", email: "user6@testeLinkedin.com" },
-];
-
 const express = require("express");
-const { usersFB } = require("./utils/facebookUsers");
+const { fakeUsers } = require("./utils/fakeUsers");
 
 const app = express();
 // parse requests of content-type - application/json
 app.use(express.json());
 
 // simple route for Facebook
-app.get("/facebook/", (req, res) => {
-  if (req.query["search"]) {
-    res.json(usersFB.filter((user) => user.name.includes(req.query["search"])));
-  } else {
-    res.json(usersFB);
-  }
-});
-
-// simple route for LinkedIn
-app.get("/linkedin/", (req, res) => {
+app.get("/customAPI/", (req, res) => {
   if (req.query["search"]) {
     res.json(
-      usersLinkedin.filter((user) => user.name.includes(req.query["search"]))
+      fakeUsers.filter((user) => user.name.includes(req.query["search"]))
     );
   } else {
-    res.json(usersLinkedin);
+    res.json(fakeUsers);
   }
 });
 
--- externalApi/utils/facebookUsers.js
+++
@@ -1,8 +0,0 @@
-export const usersFB = [
-  { name: "user1", email: "user1@testeFB.com" },
-  { name: "_user2", email: "user2@testeFB.com" },
-  { name: "_user3", email: "user3@testeFB.com" },
-  { name: "_user4", email: "user4@testeFB.com" },
-  { name: "user5", email: "user5@testeFB.com" },
-  { name: "_user6", email: "user6@testeFB.com" },
-];
