622adb91dc3817ad30d738a0ebd5d4d4958f78a5
Sofia Canelas <1200185@isep.ipp.pt>
2023-06-13 00:26:47 +0100

    #4 API Update, Simulation of Facebook and LinkedIn Users

--- externalApi/index.js
+++ externalApi/index.js
@@ -1,18 +1,101 @@
 const express = require("express");
-const { fakeUsers } = require("./utils/fakeUsers");
+const fakeUsersFacebook = require("./utils/fakeUsersFacebook.json");
+const fakeUsersLinkedIn = require("./utils/fakeUsersLinkedIn.json");
 
 const app = express();
 // parse requests of content-type - application/json
 app.use(express.json());
 
-// simple route for Facebook
-app.get("/customAPI/", (req, res) => {
-  if (req.query["search"]) {
-    res.json(
-      fakeUsers.filter((user) => user.name.includes(req.query["search"]))
+const buildCorrectParams = (ageRange = "", career = "", hobbies = "") => {
+  const finalAgeRange = ageRange ? ageRange.split(",") : [0, 0];
+  const finalCareer = career ? career.split(",") : [];
+  const finalHobbies = hobbies ? hobbies.split(",") : [];
+  return {
+    finalAgeRange,
+    finalCareer,
+    finalHobbies,
+  };
+};
+
+// simple route mocking Facebook's API
+app.get("/api/simulatingFacebook/getUsers", (req, res) => {
+  let { ageRange, gender, location, career, hobbies } = req.query;
+
+  if (ageRange || gender || location || career || hobbies) {
+    let { finalAgeRange, finalCareer, finalHobbies } = buildCorrectParams(
+      ageRange || "",
+      career || "",
+      hobbies || ""
     );
+
+    const filteredUsers = fakeUsersFacebook.filter((user) => {
+      if (
+        user.age &&
+        user.age > finalAgeRange[0] &&
+        user.age < finalAgeRange[1]
+      ) {
+        return true;
+      }
+
+      // gender.split(" ")[0] will split the "Male only" into "Male" for example, and "Female only" into "Female"
+      if (
+        (user.gender && user.gender === (gender || "").split(" ")[0]) ||
+        gender === "No preference"
+      ) {
+        return true;
+      }
+
+      if (user.location?.includes(location)) {
+        return true;
+      }
+
+      // intersect the user's and the criteria's career to see if they intersect (the user has one of the selected careers)
+      const filteredUserCareer = finalCareer.filter((value) =>
+        user.career?.includes(value)
+      );
+      if (filteredUserCareer.length > 0) {
+        return true;
+      }
+
+      // intersect the user's and the criteria's hobbies to see if they intersect (the user has one or more of the selected hobbies)
+      const filteredUserHobbies = finalHobbies.filter((value) =>
+        user.hobbies?.includes(value)
+      );
+      if (filteredUserHobbies.length > 0) {
+        return true;
+      }
+    });
+
+    res.json(filteredUsers);
   } else {
-    res.json(fakeUsers);
+    res.json(fakeUsersFacebook);
+  }
+});
+
+// simple route mocking LinkedIn's API
+app.get("/api/simulatingLinkedIn/getUsers", (req, res) => {
+  let { location, career } = req.query;
+
+  if (location || career) {
+    const finalCareer = career ? career.split(",") : [];
+
+    const filteredUsers = fakeUsersLinkedIn.filter((user) => {
+      if (user.location?.includes(location)) {
+        return true;
+      }
+
+      // intersect the user's and the criteria's career to see if they intersect (the user has one of the selected careers)
+      const filteredUserCareer = finalCareer.filter((value) =>
+        user.career?.includes(value)
+      );
+      if (filteredUserCareer.length > 0) {
+        return true;
+      }
+    });
+
+    res.json(filteredUsers);
+  } else {
+    res.json(fakeUsersLinkedIn);
   }
 });
 
